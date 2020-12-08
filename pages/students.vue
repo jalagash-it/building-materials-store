d478c585d1068840691bba99440991a442897dbd
o.malikov <o.malikov@p-s.kz>
2020-12-09 01:10:35 +0600

    added github account as list item

--- pages/students.vue
+++ pages/students.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
-        karnurmax <br>
-        <!-- zhassulan96 <br>
-        zhussupov92 <br> -->
-        
+      <li class="list">
+        <h1 class="title">
+          karnurmax
+        </h1>
+      </li>
+      <li>
+        <h1 class="title">
+          oljazzz 
+        </h1>
+      </li>
+      <!-- zhassulan96 <br>
+      zhussupov92 <br> -->
     </div>
 </template>
