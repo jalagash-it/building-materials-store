7d5f1551552746956eb8ddcc8fa02f6616db17ae
Zhussipov Meirambek <38666677+bek-app@users.noreply.github.com>
2020-12-13 14:04:55 +0600

    kazakh tili kosyldy

--- src/pages/account/login.vue
+++ src/pages/account/login.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <PageHeader
-            title="My Account"
+        
+            :title="$t('auth.MyAccount')"
+            
             :breadcrumb="[
-                {title: 'Home', url: ''},
-                {title: 'Breadcrumb', url: ''},
-                {title: 'My Account', url: ''},
+                {title: $t('auth.homePage'), url: ''},
+                 {title: 'Breadcrumb', url: ''},
+                 {title: $t('auth.MyAccount'), url: ''},
+
+               
+                
             ]"
         />
 
@@ -15,29 +20,47 @@
                     <div class="col-md-6 d-flex flex-column">
                         <div class="card flex-grow-1 mb-md-0">
                             <div class="card-body">
+
                                 <h3 class="card-title">
-                                    Login
-                                </h3>
+                              
+                                                          
+                                 
+                                  {{$t('auth.signIn')}}
+                                    
+                   
+                                                                   
+                                 </h3>
+
                                 <form>
                                     <div class="form-group">
-                                        <label for="login-email">Email address</label>
+                                        <label for="login-email">
+                                        {{$t('auth.Email')}}
+
+                                         </label>
                                         <input
                                             id="login-email"
                                             class="form-control"
                                             type="email"
-                                            placeholder="Enter email"
+                                               :placeholder="$t('auth.Email')"
                                         >
                                     </div>
                                     <div class="form-group">
-                                        <label for="login-password">Password</label>
+                                        <label for="login-password">  
+                                                                    
+                                          {{$t('auth.Password')}}
+                                            
+                                    </label>
                                         <input
                                             id="login-password"
                                             class="form-control"
                                             type="password"
-                                            placeholder="Password"
+                                                :placeholder="$t('auth.Password')"
                                         >
                                         <small class="form-text text-muted">
-                                            <a href="">Forgotten Password</a>
+                                            <a href="">
+                                            {{$t('auth.ForgottenPassword')}}
+                                            
+                                            </a>
                                         </small>
                                     </div>
                                     <div class="form-group">
@@ -53,11 +76,14 @@
                                                     <Check9x7Svg class="input-check__icon" />
                                                 </span>
                                             </span>
-                                            <label class="form-check-label" for="login-remember">Remember Me</label>
+                                            <label class="form-check-label" for="login-remember">
+                                            {{$t('auth.rememberMe')}}
+
+                                            </label>
                                         </div>
                                     </div>
                                     <button type="submit" class="btn btn-primary mt-4">
-                                        Login
+                                        {{$t('auth.signIn')}}
                                     </button>
                                 </form>
                             </div>
@@ -67,38 +93,41 @@
                         <div class="card flex-grow-1 mb-0">
                             <div class="card-body">
                                 <h3 class="card-title">
-                                    Register
+                                {{$t('auth.signUp')}}
+                                  
                                 </h3>
                                 <form>
                                     <div class="form-group">
-                                        <label for="register-email">Email address</label>
+                                        <label for="register-email">                                        
+                                        {{$t('auth.Email')}}
+                                    </label>
                                         <input
                                             id="register-email"
                                             class="form-control"
                                             type="email"
-                                            placeholder="Enter email"
+                                            :placeholder="$t('auth.Email')"
                                         >
                                     </div>
                                     <div class="form-group">
-                                        <label for="register-password">Password</label>
+                                        <label for="register-password"> {{$t('auth.Password')}}</label>
                                         <input
                                             id="register-password"
                                             class="form-control"
                                             type="password"
-                                            placeholder="Password"
+                                            :placeholder="$t('auth.Password')"
                                         >
                                     </div>
                                     <div class="form-group">
-                                        <label for="register-confirm">Repeat Password</label>
+                                        <label for="register-confirm"> {{$t('auth.repeatPassword')}}</label>
                                         <input
                                             id="register-confirm"
                                             class="form-control"
                                             type="password"
-                                            placeholder="Password"
+                                            :placeholder="$t('auth.Password')"
                                         >
                                     </div>
                                     <button type="submit" class="btn btn-primary mt-4">
-                                        Register
+                                    {{$t('auth.signUp')}}
                                     </button>
                                 </form>
                             </div>
@@ -115,6 +144,7 @@
 import { Vue, Component } from 'vue-property-decorator'
 import PageHeader from '~/components/shared/page-header.vue'
 import Check9x7Svg from '~/svg/check-9x7.svg'
+ 
 
 @Component({
     components: { PageHeader, Check9x7Svg },
